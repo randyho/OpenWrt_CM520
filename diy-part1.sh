e0db3c90ab77d3d035af15001f43a47dfcfca7b0
Randy <randyho@qq.com>
2020-11-04 18:58:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add luci-theme-argon
 rm -rf lede/package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git
+# git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
