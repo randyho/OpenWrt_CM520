c7ee654bbbe98bdfd82794338e0d68bf65f389a5
Randy <randyho@qq.com>
2021-02-04 17:22:50 +0800

    Update diy-part1.sh
    add passwall

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 # Add luci-theme-argon
 rm -rf lede/package/lean/luci-theme-argon
