ba0f05e476b94202ab483003a7b499140643e827
Randy <randyho@qq.com>
2021-04-25 21:47:26 +0800

    Update diy-part1-vssr.sh

--- diy-part1-vssr.sh
+++ diy-part1-vssr.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
+sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
 
 # Add luci-theme-argon
 rm -rf lede/package/lean/luci-theme-argon
