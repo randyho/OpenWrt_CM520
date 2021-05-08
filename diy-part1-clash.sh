b5876cd511fbf3a56beb1f9ebcb544c209a0de7a
Randy <randyho@qq.com>
2021-05-09 07:54:23 +0800

    Update diy-part1-clash.sh

--- diy-part1-clash.sh
+++ diy-part1-clash.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git luci-app-openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
 # Add luci-theme-argon
 rm -rf lede/package/lean/luci-theme-argon
