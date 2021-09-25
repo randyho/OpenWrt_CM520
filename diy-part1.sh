2be156d861fcde1d2a5d2c1c35e3c25afd75a5d9
Randy <randyho@qq.com>
2021-09-25 17:41:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
@@ -28,3 +25,10 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 #添加额外软件包
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
+svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
+svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
+svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
+svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
+svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
