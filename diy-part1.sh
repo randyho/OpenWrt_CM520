677f6601d43cfbf735f119b162a036dd5b488c66
Randy <randyho@qq.com>
2021-09-27 10:55:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-th
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #添加额外软件包
-# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 
 svn co https://github.com/fw876/helloworld/trunk/tcping package/tcping
