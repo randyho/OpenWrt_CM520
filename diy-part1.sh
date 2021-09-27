431d4df2035b5a816131bbbfd36023d1cdb8e61f
Randy <randyho@qq.com>
2021-09-28 05:35:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,17 +20,18 @@
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
 rm -rf package/lean/luci-theme-argon/
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #添加额外软件包
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 
-svn co https://github.com/fw876/helloworld/trunk/tcping package/tcping
-svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
-svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
-svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
-svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
-svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
-svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+# ssr plus
+# svn co https://github.com/fw876/helloworld/trunk/tcping package/tcping
+# svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
+# svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
+# svn co https://github.com/fw876/helloworld/trunk/shadowsocks-rust package/shadowsocks-rust
+# svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/shadowsocksr-libev
+# svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
+# svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
+# svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
