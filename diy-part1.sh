784bf9dd56b779aaecd10c9ade225c562bf6b272
Randy <randyho@qq.com>
2022-06-06 10:02:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-sed -i '$a src-git cups https://github.com/TheMMcOfficial/lede-cups.git' feeds.conf.default
-
+# sed -i '$a src-git cups https://github.com/TheMMcOfficial/lede-cups.git' feeds.conf.default
+sed -i '$a src-git cups https://github.com/Gr4ffy/lede-cups.git' feeds.conf.default
 
 # Add luci-theme-argon
 # rm -rf lede/package/lean/luci-theme-argon
