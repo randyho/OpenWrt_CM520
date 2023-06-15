117e9e04765359b567a157a2e199e9d7593f8d31
Randy <randyho@outlook.com>
2023-06-15 08:35:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,11 +21,11 @@
 # rm -rf lede/package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
 rm -rf package/lean/luci-theme-argon/
-git clone -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #添加额外软件包
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # ssr plus
